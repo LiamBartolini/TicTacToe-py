019b541950df4569fb437ad903f89114cfc33e41
LiamBartolini <liambartolini@gmail.com>
2021-04-06 18:03:27 +0200

    pearsone move

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Primo esercizio py')
+print('tic tac toe')
+field = [['-', '-', '-'],
+         ['-', '-', '-'],
+         ['-', '-', '-']]
+         
+def printField():
+    ret = "\n"
+    for i in range(3):
+        for j in range(3):
+            ret += f'{field[i][j]}    '
+        ret += "\n"
+    return ret
+
+def pearsoneMove():
+    pos = list(map(int,input('Input your position (x,y) - ').strip().split(',')))
+    x = pos[0]
+    y = pos[1]
+    if x >= 0 and x < 3 and y >= 0 and y < 3:
+        if (field[x][y] == '-'):
+            field[x][y] = 'X'
+        else:
+            print(f'({x},{y}) invalid position!')
+   
+pearsoneMove()
+print(printField())
